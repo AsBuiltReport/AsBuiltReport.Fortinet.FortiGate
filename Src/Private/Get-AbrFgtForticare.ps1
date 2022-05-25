db8b1c0d5f3beba37b1cf46b4de3f4b0bb5fa33e
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-05-25 08:56:41 +0200

    Forticare: Add if Statement before report

--- Src/Private/Get-AbrFgtForticare.ps1
+++ Src/Private/Get-AbrFgtForticare.ps1
@@ -28,80 +28,83 @@ function Get-AbrFgtForticare {
 
         $Forticare = (Get-FGTMonitorLicenseStatus).forticare
 
-        Section -Style Heading2 'FortiCare' {
-            Paragraph "The following section details Forticare settings configured on Fortigate."
-            BlankLine
+        if ($Forticare) {
+            Section -Style Heading2 'FortiCare' {
+                Paragraph "The following section details Forticare settings configured on Fortigate."
+                BlankLine
 
-            $OutObj = @()
+                $OutObj = @()
 
-            $Serial = $DefaultFGTConnection.serial
+                $Serial = $DefaultFGTConnection.serial
 
-            $OutObj = [pscustomobject]@{
-                "Model"   = $Model
-                "Serial"  = $Serial
-                "Status"  = $Forticare.status
-                "Account" = $Forticare.account.ToLower()
-                "Company" = $Forticare.company
-            }
-
-            $TableParams = @{
-                Name = "FortiCare"
-                List = $true
-            }
-
-            $OutObj | Table @TableParams
-
-            Paragraph "The following section details support settings configured on Fortigate."
-            BlankLine
-            $ExpiresHW = (($Forticare | Select-Object -ExpandProperty support).hardware).expires
-            $SupportHW = [pscustomobject]@{
-                "Type"            = "Hardware"
-                "Level"           = $Forticare.support.hardware.support_level
-                "Status"          = $Forticare.support.hardware.status
-                "Expiration Date" = (Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($ExpiresHW)) | Get-Date -Format dd/MM/yyyy
-            }
-            $ExpiresEn = (($Forticare | Select-Object -ExpandProperty support).enhanced).expires
-            $SupportEn = [pscustomobject]@{
-                "Type"            = "Enhanced"
-                "Level"           = $Forticare.support.enhanced.support_level
-                "Status"          = $Forticare.support.enhanced.status
-                "Expiration Date" = (Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($ExpiresEn)) | Get-Date -Format dd/MM/yyyy
-            }
-
-            $TableParams = @{
-                Name = "Support"
-                List = $false
-            }
-
-            $Support = @()
-            $Support += $SupportHW
-            $Support += $SupportEn
-            $Support | Table @TableParams
-        }
-
-        Paragraph "The following section details firmware information on Fortigate."
-        BlankLine
-
-        $firmware = Get-FGTMonitorSystemFirmware
-        $firmware_upgrade_paths = Get-FGTMonitorSystemFirmware -upgrade_paths
-        $FortiOS = $firmware.current
-        $CurrentVersion = [version]"$($firmware.current.major).$($firmware.current.minor).$($firmware.current.patch)"
-
-        $FullUpdate = $firmware.available | Select-Object version -First 1
-        if ($FullUpdate) {
-
-            $FullUpdateVersion = [version]"$(($firmware.available | Select-Object -First 1).major).$(($firmware.available | Select-Object -First 1).minor).$(($firmware.available | Select-Object -First 1).patch)"
-
-            #Same (or greater) version, No Update Available
-            if ($CurrentVersion -ge $FullUpdateVersion) {
-                $tab_upgradePath = [pscustomobject]@{
-                    "Installed"    = $($FortiOS.version)
-                    "Update"       = "No Update Available"
-                    "Upgrade Path" = "N/A"
+                $OutObj = [pscustomobject]@{
+                    "Model"   = $Model
+                    "Serial"  = $Serial
+                    "Status"  = $Forticare.status
+                    "Account" = $Forticare.account.ToLower()
+                    "Company" = $Forticare.company
                 }
+
+                $TableParams = @{
+                    Name = "FortiCare"
+                    List = $true
+                }
+
+                $OutObj | Table @TableParams
+
+                Paragraph "The following section details support settings configured on Fortigate."
+                BlankLine
+                $ExpiresHW = (($Forticare | Select-Object -ExpandProperty support).hardware).expires
+                $SupportHW = [pscustomobject]@{
+                    "Type"            = "Hardware"
+                    "Level"           = $Forticare.support.hardware.support_level
+                    "Status"          = $Forticare.support.hardware.status
+                    "Expiration Date" = (Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($ExpiresHW)) | Get-Date -Format dd/MM/yyyy
+                }
+                $ExpiresEn = (($Forticare | Select-Object -ExpandProperty support).enhanced).expires
+                $SupportEn = [pscustomobject]@{
+                    "Type"            = "Enhanced"
+                    "Level"           = $Forticare.support.enhanced.support_level
+                    "Status"          = $Forticare.support.enhanced.status
+                    "Expiration Date" = (Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($ExpiresEn)) | Get-Date -Format dd/MM/yyyy
+                }
+
+                $TableParams = @{
+                    Name = "Support"
+                    List = $false
+                }
+
+                $Support = @()
+                $Support += $SupportHW
+                $Support += $SupportEn
+                $Support | Table @TableParams
             }
-            else {
-                <# Search only last firmware on the same Branch
+
+            $firmware = Get-FGTMonitorSystemFirmware
+            $firmware_upgrade_paths = Get-FGTMonitorSystemFirmware -upgrade_paths
+
+            if ($firmware -and $firmware_upgrade_paths) {
+                Paragraph "The following section details firmware information on Fortigate."
+                BlankLine
+
+                $FortiOS = $firmware.current
+                $CurrentVersion = [version]"$($firmware.current.major).$($firmware.current.minor).$($firmware.current.patch)"
+
+                $FullUpdate = $firmware.available | Select-Object version -First 1
+                if ($FullUpdate) {
+
+                    $FullUpdateVersion = [version]"$(($firmware.available | Select-Object -First 1).major).$(($firmware.available | Select-Object -First 1).minor).$(($firmware.available | Select-Object -First 1).patch)"
+
+                    #Same (or greater) version, No Update Available
+                    if ($CurrentVersion -ge $FullUpdateVersion) {
+                        $tab_upgradePath = [pscustomobject]@{
+                            "Installed"    = $($FortiOS.version)
+                            "Update"       = "No Update Available"
+                            "Upgrade Path" = "N/A"
+                        }
+                    }
+                    else {
+                        <# Search only last firmware on the same Branch
                 $BranchUpdate = $firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object version -First 1
                 if ($CurrentVersion -lt $BranchUpdateVersion) {
                     $upgradePath = "v$($CurrentVersion.Major).$($CurrentVersion.Minor).$($CurrentVersion.Build)"
@@ -122,43 +125,45 @@ function Get-AbrFgtForticare {
                     }
                 }
                 #>
-                $BranchUpdateVersion = [version]"$(($firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object -First 1).major).$(($firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object -First 1).minor).$(($firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object -First 1).patch)"
-                if (($CurrentVersion -lt $FullUpdateVersion) -and ($BranchUpdateVersion -ne $FullUpdateVersion)) {
-                    $upgradePath = "v$($CurrentVersion.Major).$($CurrentVersion.Minor).$($CurrentVersion.Build)"
-                    $major = $CurrentVersion.Major
-                    $minor = $CurrentVersion.Minor
-                    $patch = $CurrentVersion.Build
-                    Do {
-                        $nextFirmware = $firmware_upgrade_paths | Where-Object { $_.from.major -eq $major -and $_.from.minor -eq $minor -and $_.from.patch -eq $patch } | Select-Object -First 1
-                        $major = $nextFirmware.to.major
-                        $minor = $nextFirmware.to.minor
-                        $patch = $nextFirmware.to.patch
-                        $upgradePath = $upgradePath + " -> v$($major).$($minor).$($patch)"
-                    }Until($major -eq $FullUpdateVersion.Major -and $minor -eq $FullUpdateVersion.Minor -and $patch -eq $FullUpdateVersion.Build)
-                    $tab_upgradePath = [pscustomobject]@{
-                        "Installed"    = $($FortiOS.version)
-                        "Update"       = $($FullUpdate.version)
-                        "Upgrade Path" = $upgradePath
+                        $BranchUpdateVersion = [version]"$(($firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object -First 1).major).$(($firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object -First 1).minor).$(($firmware.available | Where-Object { $_.major -eq $CurrentVersion.Major -and $_.minor -eq $CurrentVersion.Minor } | Select-Object -First 1).patch)"
+                        if (($CurrentVersion -lt $FullUpdateVersion) -and ($BranchUpdateVersion -ne $FullUpdateVersion)) {
+                            $upgradePath = "v$($CurrentVersion.Major).$($CurrentVersion.Minor).$($CurrentVersion.Build)"
+                            $major = $CurrentVersion.Major
+                            $minor = $CurrentVersion.Minor
+                            $patch = $CurrentVersion.Build
+                            Do {
+                                $nextFirmware = $firmware_upgrade_paths | Where-Object { $_.from.major -eq $major -and $_.from.minor -eq $minor -and $_.from.patch -eq $patch } | Select-Object -First 1
+                                $major = $nextFirmware.to.major
+                                $minor = $nextFirmware.to.minor
+                                $patch = $nextFirmware.to.patch
+                                $upgradePath = $upgradePath + " -> v$($major).$($minor).$($patch)"
+                            }Until($major -eq $FullUpdateVersion.Major -and $minor -eq $FullUpdateVersion.Minor -and $patch -eq $FullUpdateVersion.Build)
+                            $tab_upgradePath = [pscustomobject]@{
+                                "Installed"    = $($FortiOS.version)
+                                "Update"       = $($FullUpdate.version)
+                                "Upgrade Path" = $upgradePath
+                            }
+                        }
                     }
                 }
+                else {
+
+                    #No $firmware.available info (no FortiCare/FortiGuard ?)
+                    $tab_upgradePath = [pscustomobject]@{
+                        "Installed"    = $($FortiOS.version)
+                        "Update"       = "N/A"
+                        "Upgrade Path" = "N/A"
+                    }
+                }
+
+                $TableParams = @{
+                    Name = "Firmware"
+                    List = $true
+                }
+
+                $tab_upgradePath | Table @TableParams
             }
         }
-        else {
-
-            #No $firmware.available info (no FortiCare/FortiGuard ?)
-            $tab_upgradePath = [pscustomobject]@{
-                "Installed"    = $($FortiOS.version)
-                "Update"       = "N/A"
-                "Upgrade Path" = "N/A"
-            }
-        }
-
-        $TableParams = @{
-            Name = "Firmware"
-            List = $true
-        }
-
-        $tab_upgradePath | Table @TableParams
     }
 
     end {
