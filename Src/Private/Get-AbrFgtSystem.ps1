efde06e1f2542122b50f3339447743cf69335ec3
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-05-30 07:50:43 +0200

    system: Add check value  before add section

--- Src/Private/Get-AbrFgtSystem.ps1
+++ Src/Private/Get-AbrFgtSystem.ps1
@@ -32,219 +32,235 @@ function Get-AbrFgtSystem {
 
             $info = Get-FGTSystemGlobal |
 
-            Section -Style Heading3 'Global' {
-                $OutObj = @()
+            if ($info) {
+                Section -Style Heading3 'Global' {
+                    $OutObj = @()
 
-                if ($info.'daily-restart' -eq "enable") {
-                    $reboot = "Everyday at $($info.'restart-time')"
-                }
-                else {
-                    $reboot = "disable"
-                }
+                    if ($info.'daily-restart' -eq "enable") {
+                        $reboot = "Everyday at $($info.'restart-time')"
+                    }
+                    else {
+                        $reboot = "disable"
+                    }
 
-                $OutObj = [pscustomobject]@{
-                    "Nom"           = $info.'hostname'
-                    "Alias"         = $info.'alias'
-                    "Reboot"        = $reboot
-                    "Port SSH"      = $info.'admin-ssh-port'
-                    "Port HTTP"     = $info.'admin-port'
-                    "Port HTTPS"    = $info.'admin-sport'
-                    "HTTPS Rediect" = $info.'admin-https-redirect'
-                }
+                    $OutObj = [pscustomobject]@{
+                        "Nom"           = $info.'hostname'
+                        "Alias"         = $info.'alias'
+                        "Reboot"        = $reboot
+                        "Port SSH"      = $info.'admin-ssh-port'
+                        "Port HTTP"     = $info.'admin-port'
+                        "Port HTTPS"    = $info.'admin-sport'
+                        "HTTPS Rediect" = $info.'admin-https-redirect'
+                    }
 
-                $TableParams = @{
-                    Name = "Global"
-                    List = $true
-                }
+                    $TableParams = @{
+                        Name = "Global"
+                        List = $true
+                    }
 
-                $OutObj | Table @TableParams
+                    $OutObj | Table @TableParams
+                }
             }
 
             $settings = Get-FGTSystemSettings
 
-            Section -Style Heading3 'Settings' {
-                $OutObj = @()
+            if ($settings) {
+                Section -Style Heading3 'Settings' {
+                    $OutObj = @()
 
-                $OutObj = [pscustomobject]@{
-                    "OP Mode"           = $settings.opmode
-                    "Central NAT"       = $settings.'central-nat'
-                    "LLDP Reception"    = $settings.'lldp-reception'
-                    "LLDP Transmission" = $settings.'lldp-transmission'
-                    "Comments"          = $settings.comments
+                    $OutObj = [pscustomobject]@{
+                        "OP Mode"           = $settings.opmode
+                        "Central NAT"       = $settings.'central-nat'
+                        "LLDP Reception"    = $settings.'lldp-reception'
+                        "LLDP Transmission" = $settings.'lldp-transmission'
+                        "Comments"          = $settings.comments
+                    }
+
+                    $TableParams = @{
+                        Name = "Settings"
+                        List = $true
+                    }
+
+                    $OutObj | Table @TableParams
                 }
-
-                $TableParams = @{
-                    Name = "Settings"
-                    List = $true
-                }
-
-                $OutObj | Table @TableParams
             }
 
-            Section -Style Heading3 'GUI Settings' {
-                $OutObj = @()
+            if ($info -and $settings) {
+                Section -Style Heading3 'GUI Settings' {
+                    $OutObj = @()
 
-                $OutObj = [pscustomobject]@{
-                    "Language"                   = $info.language
-                    "Theme"                      = $info.'gui-theme'
-                    "IPv6"                       = $info.'gui-ipv6'
-                    "Wireless Open Security"     = $info.'gui-wireless-opensecurity'
-                    "Implicit Policy"            = $settings.'gui-implicit-policy'
-                    "Dns Database"               = $settings.'gui-dns-database'
-                    "Load Balance"               = $settings.'gui-load-balance'
-                    "Explicit Proxy"             = $settings.'gui-explicit-proxy'
-                    "Dynamic Routing"            = $settings.'gui-dynamic-routing'
-                    "Application Control"        = $settings.'gui-application-control'
-                    "IPS"                        = $settings.'gui-ips'
-                    "VPN"                        = $settings.'gui-vpn'
-                    "Wireless Controller"        = $settings.'gui-wireless-controller'
-                    "Switch Controller"          = $settings.'gui-switch-controller'
-                    "WAN Load Balancing (SDWAN)" = $settings.'gui-wan-load-balancing'
-                    "Antivirus"                  = $settings.'gui-antivirus'
-                    "Web Filter"                 = $settings.'gui-webfilter'
-                    "Video Filter"               = $settings.'gui-videofilter'
-                    "DNS Filter"                 = $settings.'gui-dnsfilter'
-                    "WAF Profile"                = $settings.'gui-waf-profile'
-                    "Allow Unnamed Policy"       = $settings.'gui-allow-unnamed-policy'
-                    "Multiple Interface Policy"  = $settings.'gui-multiple-interface-policy'
-                    "ZTNA"                       = $settings.'gui-ztna'
-                    "OT"                         = $settings.'gui-ot'
+                    $OutObj = [pscustomobject]@{
+                        "Language"                   = $info.language
+                        "Theme"                      = $info.'gui-theme'
+                        "IPv6"                       = $info.'gui-ipv6'
+                        "Wireless Open Security"     = $info.'gui-wireless-opensecurity'
+                        "Implicit Policy"            = $settings.'gui-implicit-policy'
+                        "Dns Database"               = $settings.'gui-dns-database'
+                        "Load Balance"               = $settings.'gui-load-balance'
+                        "Explicit Proxy"             = $settings.'gui-explicit-proxy'
+                        "Dynamic Routing"            = $settings.'gui-dynamic-routing'
+                        "Application Control"        = $settings.'gui-application-control'
+                        "IPS"                        = $settings.'gui-ips'
+                        "VPN"                        = $settings.'gui-vpn'
+                        "Wireless Controller"        = $settings.'gui-wireless-controller'
+                        "Switch Controller"          = $settings.'gui-switch-controller'
+                        "WAN Load Balancing (SDWAN)" = $settings.'gui-wan-load-balancing'
+                        "Antivirus"                  = $settings.'gui-antivirus'
+                        "Web Filter"                 = $settings.'gui-webfilter'
+                        "Video Filter"               = $settings.'gui-videofilter'
+                        "DNS Filter"                 = $settings.'gui-dnsfilter'
+                        "WAF Profile"                = $settings.'gui-waf-profile'
+                        "Allow Unnamed Policy"       = $settings.'gui-allow-unnamed-policy'
+                        "Multiple Interface Policy"  = $settings.'gui-multiple-interface-policy'
+                        "ZTNA"                       = $settings.'gui-ztna'
+                        "OT"                         = $settings.'gui-ot'
+                    }
+
+                    $TableParams = @{
+                        Name = "Settings"
+                        List = $true
+                    }
+
+                    $OutObj | Table @TableParams
                 }
-
-                $TableParams = @{
-                    Name = "Settings"
-                    List = $true
-                }
-
-                $OutObj | Table @TableParams
             }
 
             $dns = Get-FGTSystemDns
 
-            Section -Style Heading3 'DNS' {
-                $OutObj = @()
+            if ($dns) {
+                Section -Style Heading3 'DNS' {
+                    $OutObj = @()
 
-                $OutObj = [pscustomobject]@{
-                    "Primary"   = $dns.primary
-                    "Secondary" = $dns.secondary
-                    "Domain"    = $dns.domain.domain
-                    "Protocol"  = $dns.protocol
+                    $OutObj = [pscustomobject]@{
+                        "Primary"   = $dns.primary
+                        "Secondary" = $dns.secondary
+                        "Domain"    = $dns.domain.domain
+                        "Protocol"  = $dns.protocol
+                    }
+
+                    $TableParams = @{
+                        Name = "DNS"
+                        List = $true
+                    }
+
+                    $OutObj | Table @TableParams
                 }
-
-                $TableParams = @{
-                    Name = "DNS"
-                    List = $true
-                }
-
-                $OutObj | Table @TableParams
             }
 
             $DNSServers = Get-FGTSystemDnsServer
 
-            Section -Style Heading3 'DNS Server' {
-                $OutObj = @()
+            if ($DNSServers) {
+                Section -Style Heading3 'DNS Server' {
+                    $OutObj = @()
 
-                foreach ($DNSServer in $DNSServers) {
-                    $OutObj += [pscustomobject]@{
-                        "Name"               = $DNSServer.name
-                        "Mode"               = $DNSServer.mode
-                        "DNS Filter Profile" = $DNSServer.'dnsfilter-profile'
-                        "DOH"                = $DNSServer.doh
+                    foreach ($DNSServer in $DNSServers) {
+                        $OutObj += [pscustomobject]@{
+                            "Name"               = $DNSServer.name
+                            "Mode"               = $DNSServer.mode
+                            "DNS Filter Profile" = $DNSServer.'dnsfilter-profile'
+                            "DOH"                = $DNSServer.doh
+                        }
                     }
-                }
 
-                $TableParams = @{
-                    Name = "DNS Server"
-                    List = $false
-                }
+                    $TableParams = @{
+                        Name = "DNS Server"
+                        List = $false
+                    }
 
-                $OutObj | Table @TableParams
+                    $OutObj | Table @TableParams
+                }
             }
 
             $Admins = Get-FGTSystemAdmin
 
-            Section -Style Heading3 'Admin' {
-                $OutObj = @()
+            if ($Admins) {
+                Section -Style Heading3 'Admin' {
+                    $OutObj = @()
 
-                foreach ($admin in $Admins) {
+                    foreach ($admin in $Admins) {
 
-                    $trustedHosts = $admin.trusthost1
-                    $trustedHosts += $admin.trusthost2
-                    $trustedHosts += $admin.trusthost3
-                    $trustedHosts += $admin.trusthost4
-                    $trustedHosts += $admin.trusthost5
-                    $trustedHosts += $admin.trusthost6
-                    $trustedHosts += $admin.trusthost7
-                    $trustedHosts += $admin.trusthost8
-                    $trustedHosts += $admin.trusthost9
-                    $trustedHosts += $admin.trusthost10
+                        $trustedHosts = $admin.trusthost1
+                        $trustedHosts += $admin.trusthost2
+                        $trustedHosts += $admin.trusthost3
+                        $trustedHosts += $admin.trusthost4
+                        $trustedHosts += $admin.trusthost5
+                        $trustedHosts += $admin.trusthost6
+                        $trustedHosts += $admin.trusthost7
+                        $trustedHosts += $admin.trusthost8
+                        $trustedHosts += $admin.trusthost9
+                        $trustedHosts += $admin.trusthost10
 
-                    $OutObj += [pscustomobject]@{
-                        "Name"          = $admin.name
-                        "Profile"       = $admin.accprofile
-                        "Trusted Hosts" = $trustedHosts
-                        "MFA"           = $admin.'two-factor'
+                        $OutObj += [pscustomobject]@{
+                            "Name"          = $admin.name
+                            "Profile"       = $admin.accprofile
+                            "Trusted Hosts" = $trustedHosts
+                            "MFA"           = $admin.'two-factor'
+                        }
                     }
-                }
 
-                $TableParams = @{
-                    Name = "Administrator"
-                    List = $false
-                }
+                    $TableParams = @{
+                        Name = "Administrator"
+                        List = $false
+                    }
 
-                $OutObj | Table @TableParams
+                    $OutObj | Table @TableParams
+                }
             }
 
             $interfaces = Get-FGTSystemInterface
 
-            Section -Style Heading3 'Interfaces' {
-                $OutObj = @()
+            if ($interfaces) {
+                Section -Style Heading3 'Interfaces' {
+                    $OutObj = @()
 
-                foreach ($interface in $interfaces) {
-                    $OutObj += [pscustomobject]@{
-                        "Name"         = $interface.name
-                        "Alias"        = $interface.alias
-                        "Role"         = $interface.role
-                        "Description"  = $interface.description
-                        "Type"         = $interface.type
-                        "Vlan ID"      = $interface.vlanid
-                        "Mode"         = $interface.mode
-                        "IP Address"   = $interface.ip.Replace(' ', '/')
-                        "Allow Access" = $interface.allowaccess
-                        'DHCP Relais'  = $interface.'dhcp-relay-ip'
-                        "Status"       = $interface.status
-                        "Speed"        = $interface.speed
+                    foreach ($interface in $interfaces) {
+                        $OutObj += [pscustomobject]@{
+                            "Name"         = $interface.name
+                            "Alias"        = $interface.alias
+                            "Role"         = $interface.role
+                            "Description"  = $interface.description
+                            "Type"         = $interface.type
+                            "Vlan ID"      = $interface.vlanid
+                            "Mode"         = $interface.mode
+                            "IP Address"   = $interface.ip.Replace(' ', '/')
+                            "Allow Access" = $interface.allowaccess
+                            'DHCP Relais'  = $interface.'dhcp-relay-ip'
+                            "Status"       = $interface.status
+                            "Speed"        = $interface.speed
+                        }
                     }
-                }
 
-                $TableParams = @{
-                    Name = "Interface"
-                    List = $false
-                }
+                    $TableParams = @{
+                        Name = "Interface"
+                        List = $false
+                    }
 
-                $OutObj | Table @TableParams
+                    $OutObj | Table @TableParams
+                }
             }
 
             $zones = Get-FGTSystemZone
 
-            Section -Style Heading3 'Zone' {
-                $OutObj = @()
+            if ($zones) {
+                Section -Style Heading3 'Zone' {
+                    $OutObj = @()
 
-                foreach ($zone in $zones) {
-                    $OutObj += [pscustomobject]@{
-                        "Name"        = $zone.name
-                        "Intrazone"   = $zone.intrazone
-                        "Interface"   = $zone.interface.'interface-name'
-                        "Description" = $zone.description
+                    foreach ($zone in $zones) {
+                        $OutObj += [pscustomobject]@{
+                            "Name"        = $zone.name
+                            "Intrazone"   = $zone.intrazone
+                            "Interface"   = $zone.interface.'interface-name'
+                            "Description" = $zone.description
+                        }
                     }
-                }
 
-                $TableParams = @{
-                    Name = "Zone"
-                    List = $false
-                }
+                    $TableParams = @{
+                        Name = "Zone"
+                        List = $false
+                    }
 
-                $OutObj | Table @TableParams
+                    $OutObj | Table @TableParams
+                }
             }
 
         }
