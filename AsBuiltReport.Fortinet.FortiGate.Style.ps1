76af56f0e338b484ecf0e84fb84fac930363d534
Tim Carman <tpcarman@gmail.com>
2023-05-16 16:52:05 +1000

    Update module manifest and GitHub templates

--- AsBuiltReport.Fortinet.FortiGate.Style.ps1
+++ AsBuiltReport.Fortinet.FortiGate.Style.ps1
@@ -11,7 +11,9 @@ Style -Name 'Heading 1' -Size 16 -Color 'DA291C'
 Style -Name 'Heading 2' -Size 14 -Color 'DA291C'
 Style -Name 'Heading 3' -Size 12 -Color 'DA291C'
 Style -Name 'Heading 4' -Size 11 -Color 'DA291C'
+Style -Name 'NO TOC Heading 4' -Size 11 -Color 'DA291C'
 Style -Name 'Heading 5' -Size 10 -Color 'DA291C'
+Style -Name 'NO TOC Heading 5' -Size 10 -Color 'DA291C'
 Style -Name 'Normal' -Size 10 -Color '565656' -Default
 Style -Name 'Caption' -Size 10 -Color '565656' -Italic -Align Center
 Style -Name 'Header' -Size 10 -Color '565656' -Align Center
